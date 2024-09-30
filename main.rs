c18e697d98e34383c72f5a65f037d3d3f60ece5f
brianBasco <33673873+brianBasco@users.noreply.github.com>
2024-09-30 21:42:10 +0000

    additions simples

---
+++ main.rs
@@ -0,0 +1,19 @@
+
+fn main() {
+    println!("hello world");
+
+    let a:u8 = 26;
+    let b:u8 = 14;
+
+    let res:u8 = a+b;
+
+    println!("le résultat de a + b est : {}",res);
+
+    let res2:u8 = res;
+
+    println!("res =  : {}",res);
+    println!("res2 =  : {}",res2);
+
+    // Faire une partie avec les variables mutables cette fois :
+
+}
